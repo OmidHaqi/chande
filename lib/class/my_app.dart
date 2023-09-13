d7ed4c45d5639092ffa3c283798615c1288ea73a
Omid Haqi <omid.haqi@outlook.com>
2023-09-13 17:34:07 +0330

    abb appBar

--- lib/class/my_app.dart
+++ lib/class/my_app.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("reBase App")),
-        body:  const Center(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          actions: [
+            const SizedBox(width: 8),
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Image.asset("assets/images/menu_icon.png"),
+              ),
+            ),
+            const Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                "قیمت بروز ارز ",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Image.asset("assets/images/currency_icon.png")
+          ],
+
+          // title: Row(
+          //   children: [
+          //     Expanded(
+          //       child: Align(
+          //         alignment: Alignment.centerLeft,
+          //         child: Image.asset("assets/images/menu_icon.png"),
+          //       ),
+          //     ),
+          //     const Text("قیمت بروز ارز "),
+          //     Image.asset("assets/images/currency_icon.png")
+          //   ],
+          // ),
+          // actions: [
+          //   Icon(
+          //     Icons.menu,
+          //     color: Color(0xFFffffff),
+          //   ),
+          // ],
+        ),
+        body: const Center(
           child: Text("Hello World"),
         ),
       ),
